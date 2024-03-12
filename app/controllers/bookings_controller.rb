c15a1d489890627e279dc52437764d13ad72be4a
Giulio Avalos <giulio_avalos@hotmail.es>
2024-03-12 17:43:14 -0300

    added controller and model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,42 @@
 class BookingsController < ApplicationController
   def new
-  end
-
-  def edit
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(booking_params)
+      redirect_to @booking
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    redirect_to bookings_url
   end
 
   private
 
   def booking_params
+    params.require(:booking).permit(:date, :beginning_time, :ending_time, :court_id, :user_id)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :user
   belongs_to :court
+  belongs_to :user
+
+  validates :date, :beginning_time, :ending_time, :court_id, :user_id, presence: true
 end
