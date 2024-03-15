2580567ee6d82289ffc6895c2b1e0625fb9a74ca
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-15 14:23:27 -0300

    Add carousel to court photos and fix navbar link

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,40 @@
-require 'open-uri'
 require 'cloudinary'
+require 'faker'
 
-User.destroy_all
-Court.destroy_all
+def upload_to_cloudinary(image_path)
+  Cloudinary::Uploader.upload(image_path)['secure_url']
+end
 
-users_attributes = [
-  { email: 'test@gmail.com', password: '123456' },
-  { email: 'mike@gmail.com', password: 'KansasCity' },
-  { email: 'brandon@gmail.com', password: 'IloveKansas' },
-  { email: 'jennifer@gmail.com', password: 'LongliveAmerica' },
-  { email: 'Bob@gmail.com', password: 'AMERICAAAA' }
+users_data = [
+  { name: Faker::Name.name, email: Faker::Internet.email, password: 'password' }, # Generate data using Faker
+  { name: 'User 1', email: 'user1@example.com', password: 'password1' },
+  { name: 'User 2', email: 'user2@example.com', password: 'password2' },
 ]
-users_attributes.each do |user_attributes|
-  User.find_or_create_by!(email: user_attributes[:email]) do |user|
-    user.password = user_attributes[:password]
-  end
-end
-
-def upload_to_cloudinary(photo)
-  Cloudinary::Uploader.upload(photo)['secure_url']
-end
 
 courts_data = [
-  { name: 'Court A', address: 'Address A', description: 'Description A', price: 50.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/sample' },
-  { name: 'Court B', address: 'Address B', description: 'Description B', price: 40.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/cld-sample-2' },
-  { name: 'Court C', address: 'Address C', description: 'Description C', price: 60.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/cld-sample-4' },
-  { name: 'Court D', address: 'Address D', description: 'Description D', price: 45.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/sample' },
-  { name: 'Court E', address: 'Address E', description: 'Description E', price: 55.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/cld-sample-5' },
-  { name: 'Court F', address: 'Address F', description: 'Description F', price: 65.0, photo: 'https://res.cloudinary.com/dsmd2uryj/image/upload/f_auto,q_auto/sample' },
+  { name: 'Court A', address: 'Address A', description: 'Description A', price: 50.0, image_path: 'path_to_image1.jpg' },
+  { name: 'Court B', address: 'Address B', description: 'Description B', price: 40.0, image_path: 'path_to_image2.jpg' },
+  { name: 'Court C', address: 'Address C', description: 'Description C', price: 60.0, image_path: 'path_to_image3.jpg' },
+  { name: 'Court D', address: 'Address D', description: 'Description D', price: 45.0, image_path: 'path_to_image4.jpg' },
+  { name: 'Court E', address: 'Address E', description: 'Description E', price: 55.0, image_path: 'path_to_image5.jpg' },
 ]
-courts_data.each do |court_attrs|
-  image_url = upload_to_cloudinary(court_attrs[:image_path])
-  Court.create!(court_attrs.except(:image_path).merge(photo: image_url))
+
+shuffled_courts = courts_data.shuffle
+
+users_data.each do |user_attrs|
+  user = User.create!(user_attrs)
+
+  3.times do
+    court_attrs = shuffled_courts.pop
+    image_url = upload_to_cloudinary(court_attrs[:image_path])
+    user.courts.create!(
+      name: court_attrs[:name],
+      address: court_attrs[:address],
+      description: court_attrs[:description],
+      price: court_attrs[:price],
+      photo: image_url
+    )
+  end
 end
 
 puts "Seed data successfully created"
